f50741a6420e9be6b429e420536aa8167bbf6b21
kbarbry <kellianbarbry@gmail.com>
2024-02-18 06:15:24 +0100

    fix bug

--- src/teacher/home/home.tsx
+++ src/teacher/home/home.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { useAppSelector } from '../../store/hooks'
-import ReactAudioPlayer from 'react-audio-player'
 
 const Home: React.FC = () => {
   const { t } = useTranslation()
